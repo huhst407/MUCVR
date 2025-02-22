e34504f9012246f646cc2b965bd0162cd6fafbd7
MrChenLearnSpace <emmaalen0@gmail.com>
2025-02-22 16:14:32 +0800

    no message

--- KCPCloudServer/Assets/Script/ObjectSeparate.cs
+++ KCPCloudServer/Assets/Script/ObjectSeparate.cs
@@ -4,19 +4,17 @@ using UnityEngine;
 
 public class ObjectSeparate : MonoBehaviour
 {
-    //边缘端版
 
-    // Start is called before the first frame update
     private void OnTriggerEnter(Collider other) {
         MeshRenderer meshRenderer;
         if (other.gameObject.TryGetComponent<MeshRenderer>(out meshRenderer)) {
-            meshRenderer.enabled = true;
+            meshRenderer.enabled = false;
         }
     }
     private void OnTriggerExit(Collider other) {
         MeshRenderer meshRenderer;
         if (other.gameObject.TryGetComponent<MeshRenderer>(out meshRenderer)) {
-            meshRenderer.enabled = false;
+            meshRenderer.enabled = true;
         }
     }
 }
